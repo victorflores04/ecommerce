bb00aa936988b22f1eb878179981a0ddc40ed6d9
victor <flores.vicf@gmail.com>
2021-05-07 11:09:02 -0500

    fixed order by id endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -23,7 +23,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 //@access Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name', 'email')
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
     if (order) {
         res.json(order)
     } else {
