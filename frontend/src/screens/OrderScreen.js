7e92935db95ddd528f61cacb6a59c77b3ec04003
victor <flores.vicf@gmail.com>
2021-05-07 12:26:13 -0500

    fixed check for order

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -27,7 +27,7 @@ const OrderScreen = ({ match }) => {
         if (!order || order._id !== orderId) {
             dispatch(getOrderDetails(orderId))
         }
-    }, [order, orderId])
+    }, [order, orderId, dispatch])
 
     return (
         loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : <>
