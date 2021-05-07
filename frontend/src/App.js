384e9a71c712edb7c5f21e1c1fd3a3d0f1e111a2
victor <flores.vicf@gmail.com>
2021-05-07 11:34:54 -0500

    created Orden Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
             <Route path='/' component={HomeScreen} exact />
             <Route path='/login' component={LoginScreen} />
             <Route path='/shipping' component={ShippingScreen} />
+            <Route path='/order/:id' component={OrderScreen} />
             <Route path='/placeorder' component={PlaceOrderScreen} />
             <Route path='/payment' component={PaymentScreen} />
             <Route path='/register' component={RegisterScreen} />
--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Form, Row, Col } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Form, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { savePaymentMethod } from '../actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps'
@@ -9,7 +9,7 @@ const PaymentScreen = ({ history }) => {
 
     const cart = useSelector(state => state.cart)
     const { shippingAddress } = cart
-    if(!shippingAddress){
+    if (!shippingAddress) {
         history.push('/shipping')
     }
 
@@ -25,18 +25,18 @@ const PaymentScreen = ({ history }) => {
     }
     return (
         <FormContainer>
-            <CheckoutSteps step1 step2 step3/>
+            <CheckoutSteps step1 step2 step3 />
             <h1>Payment Method</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group>
                     <Form.Label as='legend'>Select Method</Form.Label>
-                
-                <Col>
-                    <Form.Check type='radio' label='Paypal or Credit Card' id='Paypal' name='paymentMethod' value='Paypal'
-                    checked onChange={(e)=>setPaymentMethod(e.target.value)}></Form.Check>
-                    <Form.Check type='radio' label='Stripe' id='Stripe' name='paymentMethod' value='Stripe'
-                    checked onChange={(e)=>setPaymentMethod(e.target.value)}></Form.Check>
-                </Col>
+
+                    <Col>
+                        <Form.Check type='radio' label='Paypal or Credit Card' id='Paypal' name='paymentMethod' value='Paypal'
+                            checked onChange={(e) => setPaymentMethod(e.target.value)}></Form.Check>
+                        <Form.Check type='radio' label='Stripe' id='Stripe' name='paymentMethod' value='Stripe'
+                            checked onChange={(e) => setPaymentMethod(e.target.value)}></Form.Check>
+                    </Col>
                 </Form.Group>
                 <Button type='submit' variant='primary'>Continue</Button>
             </Form>
