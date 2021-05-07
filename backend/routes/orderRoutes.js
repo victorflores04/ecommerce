9eae9432456ebd69a2964cef5498057af42a489e
victor <flores.vicf@gmail.com>
2021-05-07 15:29:17 -0500

    added show orders on profile

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,8 +4,9 @@ import { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid } from '../
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
-router.route('/myorders').put(protect, getMyOrders)
+
 
 export default router
