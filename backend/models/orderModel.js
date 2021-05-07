17206d12da81426744bf102049dee5bc0b051a86
victor <flores.vicf@gmail.com>
2021-05-07 01:21:50 -0500

    fixed model order

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    orderITems: [{
+    orderItems: [{
         name: { type: String, required: true },
         qty: { type: Number, required: true },
         price: { type: Number, required: true },
